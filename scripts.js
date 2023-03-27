437115e061efe0a9b43fc9f7c074e24f1828014f
Adam Morrison <morrison.adam.s@gmail.com>
2023-03-27 11:12:15 -0500

    testing resize

--- scripts.js
+++ scripts.js
@@ -14,3 +14,20 @@ document.addEventListener('DOMContentLoaded', () => {
   const header = document.querySelector('.header');
   header.appendChild(navButton);
 });
+
+// function for tableau embedd
+function setTableauVizSize() {
+  var divElement = document.getElementById('viz1679682472121');
+  if (!divElement) return;
+  var vizElement = divElement.getElementsByTagName('object')[0];
+  var width = divElement.offsetWidth;
+  var height = (width * 1227) / 1000;
+  vizElement.style.width = width + 'px';
+  vizElement.style.height = height + 'px';
+}
+
+// function for resize
+document.addEventListener('DOMContentLoaded', function () {
+  setTableauVizSize();
+  window.addEventListener('resize', setTableauVizSize);
+});
